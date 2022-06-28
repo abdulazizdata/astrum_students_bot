56540a2fd0215859f2f0bf56beb87f09bdd85705
abdulazizdata <106421202+abdulazizdata@users.noreply.github.com>
2022-06-28 14:00:25 +0500

    Update markups.py

--- markups.py
+++ markups.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 btn_srch_by_qw = KeyboardButton('Qwasar user boyicha qidirish')
-btn_srch_by_tg = KeyboardButton('Telegram user name boyicha qidirish')
+btn_srch_by_tg = KeyboardButton("Rasm bo'yicha qidirish")
 
 mainMenu = ReplyKeyboardMarkup(resize_keyboard= True)
 mainMenu.add(btn_srch_by_qw,btn_srch_by_tg)
